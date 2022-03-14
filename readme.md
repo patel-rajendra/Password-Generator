31d14c8172c0f6c7916540cd272bbe332a544713
rpatel-katalyst <rpatel@katalystadvantage.com>
2022-03-13 23:04:34 -0400

    Final commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Password Generator
+
+This application generates a random password based on user-selected criteria. 
+
+
+# Acceptance Criteria
+
+- Click the button to generate a password
+- It presented with a series of prompts for password criteria
+- User select which criteria to include in the password
+- The length of the password at least 8 characters and no more than 128 characters
+- Choose lowercase, uppercase, numeric, and/or special characters in the password
+- Input should be validated and at least one character type should be selected
+- A password is generated that matches the selected criteria
+- The password is displayed in written to the page
+
+# Additional requirements:
+- The app runs in the browser and features dynamically updated HTML and CSS powered by your JavaScript code.
+- It has a clean and polished user interface and is responsive, which ensures that it adapts to multiple screen sizes.
--- script.js
+++ script.js
@@ -16,6 +16,8 @@ generateBtn.addEventListener("click", writePassword);
 
 //Generate Password
 function generatePassword(){
+
+  // Variable declaration 
   var lowerChar = "abcdefghijklmnopqrstuvwxyz";
   var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var numbers = "0123456789";
@@ -24,33 +26,46 @@ function generatePassword(){
   var str = '';
 
   var numberofcharacter = window.prompt("How many characters would you like your password to contain?");
+  var number=parseInt(numberofcharacter);
+  
+  // Checking condition for the length of at least 8 characters and no more than 128 characters
+   if(number>=8 && number<=128)
+   {
   var specialcharacter = window.confirm("Click OK to confirm including special character");
   var numeric = window.confirm("Click OK to confirm numeric character");
   var lowercase = window.confirm("Click OK to confirm including lowercase characters");
   var uppercase = window.confirm("Click OK to confimr uppercase character");
-  var num=parseInt(numberofcharacter);
+  
 
-  if(specialcharacter == true)
-  {
-    str = str + symbols;
-  }
-  if(numeric == true)
-  {
-    str = str + numbers;
-  }
-  if(lowercase == true)
-  {
-    str = str + lowerChar;
-  }
-  if(uppercase == true)
-  {
-    str = str + upperChar;
-  }
+  // Conditional statement to include into password
+  
+    if(specialcharacter == true)
+    {
+     str = str + symbols;
+    }
+    if(numeric == true)
+    {
+      str = str + numbers;
+    }
+    if(lowercase == true)
+    {
+      str = str + lowerChar;
+    }
+    if(uppercase == true)
+    {
+      str = str + upperChar;
+    }
 
-  for(i=0;i<num;i++)
-  {
-    var char=Math.floor(Math.random()*str.length);
-    random_password = random_password + str.charAt(char);
+  // Looping to generate password
+    for(i=0;i<number;i++)
+    {
+      var char=Math.floor(Math.random()*str.length);
+      random_password = random_password + str.charAt(char);
+    }
+    return random_password;
   }
-  return random_password;
+  else
+  {
+    window.alert("Please enter the character between 8 to 128");
+  } 
 }
